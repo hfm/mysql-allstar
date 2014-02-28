518ff880c1446a56ae0ff212fe674989b7c88df5
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-02-28 15:15:42 +0900

    insecure付けないと死ぬ奴ある

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,9 +1,10 @@
-#!/bin/sh -e
+#!/bin/sh
+set -e
 
 PATH=/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH
 
 mkdir -p /home/vagrant/.ssh
-curl -o /home/vagrant/.ssh/authorized_keys -L 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+curl -o /home/vagrant/.ssh/authorized_keys -kL 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
 
