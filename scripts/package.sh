ae628509ddb9eece93c20ecc539dcdfea3326858
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-04-20 01:12:17 +0900

    fix for 4 and 5

--- scripts/package.sh
+++ scripts/package.sh
@@ -23,12 +23,24 @@ esac
 curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL -L "http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/RPM-GPG-KEY-EPEL-$osmajor"
 rpm -ivh "http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/$osmajor/$osarch/$epel"
 
-# puppet
-if [ $osmajor != 4 ]; then
+
+if [ $osmajor == 4 ]; then
+    curl -OLk "https://git-core.googlecode.com/files/git-1.9.0.tar.gz"
+    tar zvxf git-1.9.0.tar.gz
+    cd git-1.9.0 && ./configure && make make install
+    rm -rf /home/vagrant/git-1.9.0{,.tar.gz}
+
     curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs -L 'http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs'
     rpm -ivh "https://yum.puppetlabs.com/el/$osmajor/products/$osarch/puppetlabs-release-$osmajor-10.noarch.rpm"
+elif [ "$osmajor" == 5 ]; then
+    rpmforge=rpmforge-release-0.5.3-1.el5.rf.${osarch}.rpm
+    rpm --import -v http://apt.sw.be/RPM-GPG-KEY.dag.txt
+    curl -OL "http://pkgs.repoforge.org/rpmforge-release/${rpmforge}"
+    rpm -Kv $rpmforge
+    rpm -ivh $rpmforge
+    sed -i 's/enabled = 1/enabled = 0/' /etc/yum.repos.d/rpmforge.repo
 fi
-yum install -y puppet
+yum install -y puppet cmake
 
 # chef
 curl -kL 'https://www.opscode.com/chef/install.sh' | bash
