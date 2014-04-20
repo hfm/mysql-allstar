4692c6e5ef6d365627ad65867d81026b6937fc0c
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-04-21 05:54:26 +0900

    resolve #4

--- scripts/mysql-build.sh
+++ scripts/mysql-build.sh
@@ -5,8 +5,9 @@ PATH=/usr/local/bin:/usr/bin:/sbin:/usr/sbin:$PATH
 build_path=/home/vagrant/mysql-build
 mysql_path=/home/vagrant/mysql
 
-[ -d $build_path ] || git clone https://github.com/kamipo/mysql-build.git $build_path
-mkdir -p $mysql_path
+git clone https://github.com/kamipo/mysql-build.git $build_path
+chown -R vagrant:vagrant $build_path
+install -o vagrant -g vagrant -d $mysql_path
 
 VERS=(
     4.0.30
@@ -46,7 +47,7 @@ popd
 for ver in ${VERS[@]}; do
     pushd "${mysql_path}/${ver}"
 
-    mkdir ${mysql_path}/${ver}/etc
+    mkdir    ${mysql_path}/${ver}/etc
     mkdir -p ${mysql_path}/${ver}/var/{log,lib}/mysql
 
     cat<<EOS >${mysql_path}/${ver}/etc/my.cnf
@@ -55,8 +56,14 @@ socket = ${mysql_path}/${ver}/var/lib/mysql
 
 [mysqld]
 socket = ${mysql_path}/${ver}/var/lib/mysql
-default-character-set = utf8
 EOS
+
+    mysqlminor=$(${mysql_path}/${ver}/bin/mysql --version | awk '{print $5}' | cut -f2 -d'.')
+    if [ $mysqlminor -ge 5 ]; then
+        echo "character-set-server = utf8 " >>${mysql_path}/${ver}/etc/my.cnf
+    else
+        echo "default-character-set= utf8 " >>${mysql_path}/${ver}/etc/my.cnf
+    fi
 
     mysqlmajor=$(${mysql_path}/${ver}/bin/mysql --version | awk '{print $5}' | cut -f1 -d'.')
     if [ $mysqlmajor -eq 5 ]; then
@@ -68,6 +75,8 @@ EOS
 
     popd
 done
+
+chown -R vagrant:vagrant $mysql_path
 
 
 # setup for mysql_install_db
@@ -82,6 +91,3 @@ for ver in ${VERS[@]}; do
 
     popd
 done
-
-chown -R vagrant:vagrant $build_path
-chown -R vagrant:vagrant $mysql_path
