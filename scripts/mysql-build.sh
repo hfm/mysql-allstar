78b999db91efae5b863e0ebde8246871e2c71c76
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-04-20 17:06:12 +0900

    updated

--- scripts/mysql-build.sh
+++ scripts/mysql-build.sh
@@ -6,10 +6,7 @@ build_path=/home/vagrant/mysql-build
 mysql_path=/home/vagrant/mysql
 
 [ -d $build_path ] || git clone https://github.com/kamipo/mysql-build.git $build_path
-
-groupadd mysql
-useradd -s /sbin/nologin -g mysql mysql
-install -o vagrant -g vagrant -d $mysql_path
+mkdir -p $mysql_path
 
 VERS=(
     4.0.30
@@ -20,20 +17,56 @@ VERS=(
     5.6.17
 )
 
+cat<<'EOS' >/etc/my.cnf
+[client]
+user = vagrant
+
+[mysqld]
+user = vagrant
+
+[mysqlhotcopy]
+interactive-timeout
+
+[mysqldump]
+quick
+EOS
+
 pushd $build_path
 for ver in ${VERS[@]}; do
     ./bin/mysql-build $ver "${mysql_path}/${ver}"
 done
 popd
 
-pushd "${mysql_path}/${ver}"
 for ver in ${VERS[@]}; do
-    mkdir "${mysql_path}/${ver}/etc"
-    mkdir -p "${mysql_path}/${ver}/{log,lib}/mysql"
-    ./bin/mysql_install_db
-    ./scripts/mysql_install_db
+    pushd "${mysql_path}/${ver}"
+
+    mkdir ${mysql_path}/${ver}/etc
+    mkdir -p ${mysql_path}/${ver}/var/{log,lib}/mysql
+
+    if [ -f "bin/mysql_install_db" ]; then
+        cat<<EOS >${mysql_path}/${ver}/etc/my.cnf
+[client]
+socket = ${mysql_path}/${ver}/var/lib/mysql
+
+[mysqld]
+socket = ${mysql_path}/${ver}/var/lib/mysql
+default-character-set = utf8
+EOS
+        ./bin/mysql_install_db
+    else
+        cat<<EOS >${mysql_path}/${ver}/etc/my.cnf
+[client]
+socket = ${mysql_path}/${ver}/var/lib/mysql
+
+[mysqld]
+socket = ${mysql_path}/${ver}/var/lib/mysql
+character-set-server = utf8
+EOS
+        ./scripts/mysql_install_db
+    fi
+
+    popd
 done
-popd
 
 chown -R vagrant:vagrant $build_path
 chown -R vagrant:vagrant $mysql_path
